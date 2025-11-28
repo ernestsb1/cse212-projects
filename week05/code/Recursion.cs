c6df575655f166e3f7e80cb003e7282f12bc5bfe
ernestsb1 <ernestnyore2020@gmail.com>
2025-11-28 13:33:53 +0100

    problem resolve

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,9 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+            return 0;
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -40,6 +42,17 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char letter = letters[i];
+            string remainingLetters = letters.Remove(i, 1);
+            PermutationsChoose(results, remainingLetters, size, word + letter);
+        }
     }
 
     /// <summary>
@@ -97,11 +110,30 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+   if (remember == null)
+            remember = new Dictionary<int, decimal>();
 
+        // Base cases
+        if (s == 0) return 0;
+        if (s == 1) return 1;
+        if (s == 2) return 2;
+        if (s == 3) return 4;
+
+        // If memo already contains answer, return it
+        if (remember.ContainsKey(s))
+            return remember[s];
+        
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways =
+            CountWaysToClimb(s - 1, remember) +
+            CountWaysToClimb(s - 2, remember) +
+            CountWaysToClimb(s - 3, remember);
+
+        remember[s] = ways;   // store memo
         return ways;
     }
+
+       
 
     /// <summary>
     /// #############
@@ -119,6 +151,21 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+         int index = pattern.IndexOf('*');
+
+        // No wildcard → final string
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        string p0 = pattern[..index] + "0" + pattern[(index + 1)..];
+        WildcardBinary(p0, results);
+
+        string p1 = pattern[..index] + "1" + pattern[(index + 1)..];
+        WildcardBinary(p1, results);
+    
     }
 
     /// <summary>
@@ -129,15 +176,37 @@ public static class Recursion
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
-            currPath = new List<ValueTuple<int, int>>();
+        if (currPath == null)
+        {
+        currPath = new List<(int, int)>();
+
         }
-        
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
+
+       if (!maze.IsValidMove(currPath, x, y))
+        return; 
+
+        currPath.Add((x, y)); 
+        // // Use this syntax to add to the current path
 
         // TODO Start Problem 5
         // ADD CODE HERE
+ 
+    // If we reached the end square, add the full path to results
+    if (maze.IsEnd(x, y))
+    {
+        results.Add(currPath.AsString());  // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+        currPath.RemoveAt(currPath.Count - 1); // backtrack
+        return;
+    }
 
+    // Explore all 4 directions
+    SolveMaze(results, maze, x + 1, y, currPath); // Right
+    SolveMaze(results, maze, x - 1, y, currPath); // Left
+    SolveMaze(results, maze, x, y + 1, currPath); // Down
+    SolveMaze(results, maze, x, y - 1, currPath); // Up
+
+    // Backtrack (remove current position before returning)
+    currPath.RemoveAt(currPath.Count - 1);
+}
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
-}
