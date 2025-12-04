325956f0da8f9cb8a8a232d52a4040a60874fa83
ernestsb1 <ernestnyore2020@gmail.com>
2025-12-04 22:53:33 +0100

    Completed Problems 1-5: Node insert, contains, get height, BST traversal, and create tree from sorted list

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -9,37 +9,51 @@ public class Node
         this.Data = data;
     }
 
+    // Problem 1: Insert a value into the BST
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+              // TODO Start Problem 1
 
         if (value < Data)
         {
-            // Insert to the left
-            if (Left is null)
+            if (Left == null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else if (value> Data)
         {
-            // Insert to the right
-            if (Right is null)
+            
+            if (Right == null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
     }
 
+    // Problem 2: Search for a value
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+                // TODO Start Problem 2
+
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+
+        return Right != null && Right.Contains(value);
     }
 
+    // Problem 4: Get height of the tree
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+                // TODO Start Problem 4
+
+        int leftHeight = Left != null ? Left.GetHeight() : 0;
+        int rightHeight = Right != null ? Right.GetHeight() : 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight); // Replace this line with the correct return statement(s)
+        
     }
 }
